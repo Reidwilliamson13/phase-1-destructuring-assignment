dbcc539a09d806e79bd7e881cb3d1c7313c55780
ReidWilliamson <reidwilliamson2@gmail.com>
2021-10-06 16:46:09 -0400

    commit tree 31c9b469552d88ca0278c761e52e36c0e356ae8f

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-const farmAnimals = 'cow horse sheep pig chicken';
+const farmAnimals = "cow horse sheep pig chicken";
 
-const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 
 const muppet = {
-  muppetName: 'Miss Piggy',
-  color: 'pink',
-  song: 'Never Before, Never Again',
-  job: 'Cast member of The Muppet Show',
-  partner: 'Kermit'
+  muppetName: "Miss Piggy",
+  color: "pink",
+  song: "Never Before, Never Again",
+  job: "Cast member of The Muppet Show",
+  partner: "Kermit",
 };
 
 const nestedMuppet = {
-  nestedName: 'Kermit',
-  nestedColor: 'green',
+  nestedName: "Kermit",
+  nestedColor: "green",
   album: {
     theMuppetMovie: {
-      song1: 'Rainbow Connection',
-      song2: 'Moving Right Along',
-      song3: 'Never Before, Never Again',
-      song4: 'I Hope That Something Better Comes Along',
+      song1: "Rainbow Connection",
+      song2: "Moving Right Along",
+      song3: "Never Before, Never Again",
+      song4: "I Hope That Something Better Comes Along",
     },
   },
-  nestedJob: 'Host of The Muppet Show',
-  nestedPartner: 'Miss Piggy'
+  nestedJob: "Host of The Muppet Show",
+  nestedPartner: "Miss Piggy",
 };
 
 // Strings
@@ -39,10 +39,35 @@ const nestedMuppet = {
 
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
 
-// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
+// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg.
 
 // Objects
 
 // 7. Use destructuring to assign all variables using the keys as the variable names
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+
+//1.)
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(" ");
+console.log(moo, neigh);
+
+//2.)
+const [bessie, , dolly, babe, little] = farmAnimals.split(" ");
+console.log(bessie, dolly);
+
+//3.)
+const [blackAndWhite, , black, pink, ,] = farmAnimals.split(" ");
+
+//4.)
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+const [r, o, y, g, b, i, v] = colors;
+
+//5,6.)
+const [, , , , , indg] = colors;
+
+//7.)
+let { muppetName, color, song, job, partner } = muppet;
+
+//8.)
+let { song2, song4 } = nestedMuppet.album.theMuppetMovie;
+let { nestedJob, nestedPartner } = nestedMuppet;
